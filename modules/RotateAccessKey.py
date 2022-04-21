c26e692be536dc5e015408efff574003a4be2ee4
Milton Baltodano <mb395014@gmail.com>
2022-04-20 19:06:34 -0600

    adding module

---
+++ modules/RotateAccessKey.py
@@ -0,0 +1,33 @@
+import boto3
+
+
+class RotateAccessKey:
+    def __init__(self, access_key_id: str, secret_access_key) -> None:
+        self.__iam_client = boto3.client(
+            "iam",
+            aws_access_key_id=access_key_id,
+            aws_secret_access_key=secret_access_key,
+        )
+        self.access_key_id = access_key_id
+
+    def _create_access_key(self) -> dict:
+        response = self.__iam_client.create_access_key()
+        return response["AccessKey"]
+
+    def _delete_access_key(self) -> dict:
+        response = self.__iam_client.delete_access_key(AccessKeyId=self.access_key_id)
+        return response
+
+    def rotate_access_keys(self):
+        new_access_key = {}
+        if new_access_key := self._create_access_key():
+            self._delete_access_key()
+        return new_access_key
+
+
+# print(
+#     RotateAccessKey(
+#         access_key_id="AKIAQ2EIA7JTD7TTQM5Q",
+#         secret_access_key="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",
+#     ).rotate_access_keys()
+# )
